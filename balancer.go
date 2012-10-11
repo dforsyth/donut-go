5bff168cff87c1bd96c37b5490ef8dde94901f46
David <dforsythe@gmail.com>
2012-10-11 08:55:44 -0700

    untested count balancer

--- balancer.go
+++ balancer.go
@@ -1,5 +1,10 @@
 package donut
 
+import (
+	"sync"
+	"math"
+)
+
 type Balancer interface {
 	// Prepare the balancer to start balancing
 	Init(c *Cluster)
@@ -22,4 +27,52 @@ func (*DumbBalancer) CanClaim() bool {
 
 func (*DumbBalancer) HandoffList() []string {
 	return make([]string, 0)
+}
+
+type CountBalancer struct {
+	c *Cluster
+	lk sync.Mutex
+}
+
+func (b *CountBalancer) upTo(workCount, nodesCount int) int {
+	return int(math.Ceil(float64(workCount) / float64(nodesCount)))
+}
+
+func (b *CountBalancer) Init(c *Cluster) {
+	b.c = c
+}
+
+func (b *CountBalancer) CanClaim() bool {
+	b.lk.Lock()
+	defer b.lk.Unlock()
+
+	workCount := len(b.c.Work())
+	claimedCount := len(b.c.Claimed())
+	nodesCount := len(b.c.Nodes())
+	if workCount <= 1 && claimedCount < workCount {
+		return true
+	}
+
+	if claimedCount < b.upTo(workCount, nodesCount) {
+		return true
+	}
+
+	return false
+}
+
+func (b *CountBalancer) HandoffList() []string {
+	// for now just select from the front
+
+	work := b.c.Work()
+	workCount := len(b.c.Work())
+	claimedCount := len(b.c.Claimed())
+	nodesCount := len(b.c.Nodes())
+
+	upTo := b.upTo(workCount, nodesCount)
+	handoffCount := claimedCount - upTo
+	if handoffCount > 0 {
+		return work[:handoffCount]
+	}
+
+	return []string{}
 }
